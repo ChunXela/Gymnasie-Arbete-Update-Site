c545dc97c1bd878f56cdaede5d78e8fa1c85dae6
ChunXela <johansson.axel@protonmail.com>
2022-10-01 00:05:08 +0200

    Minor js fix, not really working

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
-document.getElementsByClassName("learnMore").onclick = function () {
-  var moreInfo = document.getElementsByClassName("moreIntresstDiv");
+function toggleText() {
+  // Get all the elements from the page
+  var points = document.getElementsByClassName("points");
 
-  if (moreInfo.style.display === "block") {
-    moreInfo.style.display = "none";
-  } else {
-    moreInfo.style.display = "block";
+  var showMoreText = document.getElementsByClassName("moreInterest");
+
+  var buttonText = document.getElementsByClassName("learnMore");
+
+  // If the display property of the dots
+  // to be displayed is already set to
+  // 'none' (that is hidden) then this
+  // section of code triggers
+  if (points.style.display === "none") {
+    // Hide the text between the span
+    // elements
+    showMoreText.style.display = "none";
+
+    // Show the dots after the text
+    points.style.display = "inline";
+
+    // Change the text on button to
+    // 'Show More'
+    buttonText.innerHTML = "Show More";
   }
-};
+
+  // If the hidden portion is revealed,
+  // we will change it back to be hidden
+  else {
+    // Show the text between the
+    // span elements
+    showMoreText.style.display = "inline";
+
+    // Hide the dots after the text
+    points.style.display = "none";
+
+    // Change the text on button
+    // to 'Show Less'
+    buttonText.innerHTML = "Show Less";
+  }
+}
